a5244728fa4f8cbddefb8de292c633bf7e439667
Nitesh Kumar Samota <samotanitesh247@gmail.com>
2026-04-15 20:42:30 +0530

    added hashpasswoed and compare method in usermodel

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -8,15 +8,34 @@ const userSchema = new mongoose.schema({
     email: {
         type: String,
         required: true,
-        unique: true
+        trim: true,
+        lowercase: true,
+        match: [/.+@.+\..+/, "Please fill a valid email address"],
+        unique: [true, "Email already exists"]
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        minlength: [6, "Password must be at least 6 characters long"],
+        select: false
     }
 }, {
     timestamps: true
 })
+
+userSchema.pre("save", async function(next){ // this refers to the password hashing process, we use pre save hook to hash the password before saving it to the database
+    if(!this.isModified("password")){
+        return next();
+    }
+    
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+    return next();
+})
+
+userSchema.methods.comparePassword = async function(password){  // this refers to the user instance, we use comparePassword method to compare the password entered by the user with the hashed password stored in the database
+    return await bcrypt.compare(password, this.password);
+}
 
 const user = mongoose.model("user",userSchema);
 
